77ded296b5d5cb9811e2b67611104b47334375d5
Jagdish2401 <jagdishhadiyal2401@gmail.com>
2025-08-06 10:51:14 +0530

    Add route for AddNewUserPage in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/DashboardPage'
 import {Routes,Route} from "react-router-dom";
 import DashboardPage from './pages/DashboardPage';
 import EmployeePage from './pages/EmployeePage';
+import AddNewUserPage from './pages/AddNewUserPage';
 import Layout from './components/Layout';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout activePage="dashboard"><DashboardPage /></Layout>} />
       <Route path="/employees" element={<Layout activePage="employees"><EmployeePage /></Layout>} />
+      <Route path="/add-new-user" element={<Layout activePage="employees"><AddNewUserPage /></Layout>} />
       <Route path="/login" element={<LoginPage />} />
     </Routes>
     </>
