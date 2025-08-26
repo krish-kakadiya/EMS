b2333fa0cb5b8fc4a1669ad0b0a4497184c4dd1e
Hardik Hadiya <hardikhadiya21@gmail.com>
2025-08-26 09:35:00 +0530

    Initialize employee management system with React and Vite, including essential components, styles, and configuration files

---
+++ employee/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import EmployeeProfile from "./pages/EmployeeProfile";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <EmployeeProfile />
+    </div>
+  );
+}
+
+export default App;
